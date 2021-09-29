4de3237268273a534625892511034a469adaccd9
Niklas Andersson <na222sn@student.lnu.se>
2021-09-29 11:46:38 +0200

    commented out google

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post(
     }
     const { country, employees, revenue, name, company, phone, email } = req.body
     res.status(200).json({ message: 'signup success' })
-
+    /*
     // Open connection to Google Sheets.
 
     const auth = new google.auth.GoogleAuth({
@@ -61,6 +61,7 @@ app.post(
         values: [[country, employees, revenue, name, company, phone, email]]
       }
     })
+    */
   })
 
 app.listen(port, () => {
